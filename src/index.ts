3bfa4006596803f7eae7138faa4fd6e7043a374a
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2022-10-01 01:47:58 -0700

    try commit hashes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { env } from "./env";
 import { octokit } from "./octokit";
 
+// Returns either a commit hash from the url or from innerHTML.
+async function getCommitHash(url: string) {
+  const sha1Regex = /\b([a-f0-9]{40})\b/;
+  const match = url.match(sha1Regex);
+  if (match) {
+    return match[0];
+  }
+  const html = await fetch(url).then((r) => r.text());
+  const baseUrlRegex = /https:\/\/github\.com\/[^\/]+\/[^\/]+/gm;
+  const baseUrl = html.match(baseUrlRegex)?.[0];
+  if (!baseUrl) {
+    console.error(`❌ Invalid github url (no base found): ${url}`);
+    return null;
+  }
+  const commitHrefRegex = new RegExp(baseUrl + "/commit." + sha1Regex);
+  const commitHref = html.match(commitHrefRegex)?.[0];
+  if (!commitHref) {
+    console.error(`❌ Invalid github url (no commit found): ${url}`);
+    return null;
+  }
+  const commitHash = commitHref.match(sha1Regex)?.[0];
+  if (!commitHash) {
+    console.error(`❌ Invalid github url (no commit hash found): ${url}`);
+    return null;
+  }
+  return commitHash;
+}
+
 async function main() {
   console.log({ env });
 
@@ -12,7 +40,11 @@ async function main() {
   // console.log({ issues });
 
   for (const issue of issues) {
-    //
+    const commitHash = await getCommitHash(issue.title);
+    if (!commitHash) {
+      continue;
+    }
+    console.log({ commitHash });
   }
 }
 
