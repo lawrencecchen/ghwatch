3ae12c017aa33485d5e77469969837a622833471
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2022-10-01 01:23:55 -0700

    proc

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import { octokit } from "./octokit";
 
 async function main() {
   console.log({ env });
+  console.log("prpoc", process.env);
 
   const issues = await octokit.paginate(octokit.rest.issues.listForRepo, {
     owner: "lawrencecchen",
     repo: "ghwatch",
+    state: "open",
   });
-  console.log({ issues });
+  // console.log({ issues });
+
+  for (const issue of issues) {
+    //
+  }
 }
 
 main();
